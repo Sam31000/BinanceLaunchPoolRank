a3590a78096e882681d3c1d03610f285a3dcc419
Sam <sami.chidiac.development@gmail.com>
2020-12-26 15:13:56 +0100

    adding structure and sample data to function asset

--- amplify/backend/function/asset/src/index.js
+++ amplify/backend/function/asset/src/index.js
@@ -1,19 +1,26 @@
+exports.handler = async (event, context, callback) => {
+    
+    console.log("EVENT : ", event.arguments.name);
+    
+    switch(event.info.fieldName) {
+        case "getAsset":
+            callback(null, getAsset(event.arguments));
+            break;
+        default:
+            callback("Unknown field, unable to resolve " + event.field, null);
+            break;
+    }
 
-
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify({
+    
+function getAsset(args){
+    if (args == null || args.name == null)
+        return null;
+    
+    return {
             name : "BUSD",
             USDValue : 1,
             imageUrl: "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20201110/4cf7d633-92fb-4d37-80ed-458c7d1ea410.png"
-        }),
-    };
-    return response;
+        };
+}
+    
 };
