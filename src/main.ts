b11797a6604ca3ff252bf539fc8ae2c777583ffa
Sam <sami.chidiac.development@gmail.com>
2020-12-26 17:39:05 +0100

    connecting backend and frontend

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import awsconfig from './aws-exports.js';
+Amplify.configure(awsconfig);
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -10,3 +14,5 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+  
