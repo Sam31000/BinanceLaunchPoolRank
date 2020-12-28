a5ff55c3353f6c420c44741ca43e8cb7b5a1c0a5
Sam <sami.chidiac.development@gmail.com>
2020-12-28 16:04:43 +0100

    initialize launchpool from binance

--- amplify/backend/function/launchPool/src/index.js
+++ amplify/backend/function/launchPool/src/index.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context, callback) => {
 
     const BASE_URL = "https://launchpad.binance.com"
     const LAUNCH_POOL_URL = BASE_URL + "/en/tab3";
-    let launchPoolLists = [];
+    const API_GETPRICE_URL = "https://api1.binance.com/api/v3/ticker/price?symbol=";
 
     switch (event.info.fieldName) {
         case "listLaunchPools":
@@ -18,59 +18,31 @@ exports.handler = async (event, context, callback) => {
 
     async function listLaunchPools() {
 
-        return await parseBinanceLaunchPoolList().then((res, err) => { return res; });
-        //console.log("RES : ", res);
-        //return launchPoolLists;
-        /* return [{
-             totalPoolStacked: 321313412.213,
-             totalPoolReward: 1424235251.213,
-             earnedAsset: {
-                 USDValue: 0.000020,
-                 name: "BNB",
-                 imageUrl: "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20201110/550eda20-1b9a-4bc7-9a65-e4a329e8bb57.png"
-             },
-             stakedAsset: {
-                 USDValue: 10,
-                 name: "CHZ",
-                 imageUrl: "https://bin.bnbstatic.com/images/20191211/721a863b-5c9d-4061-a2f0-d288dcd3d1ad.png"
-             },
-             url: "http://google.fr",
-         },
-         {
-             totalPoolStacked: 321412.213,
-             totalPoolReward: 14251.213,
-             earnedAsset: {
-                 USDValue: 0.123,
-                 name: "BTC",
-                 imageUrl: "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20201110/87496d50-2408-43e1-ad4c-78b47b448a6a.png"
-             },
-             stakedAsset: {
-                 USDValue: 10,
-                 name: "SOL",
-                 imageUrl: "https://bin.bnbstatic.com/images/20200410/6caae4f1-f4c6-4486-b05b-442dd559f51f.jpg"
-             },
-             url: "http://google.fr",
-         },
-         {
-             totalPoolStacked: 3243412.213,
-             totalPoolReward: 21515725.321,
-             earnedAsset: {
-                 USDValue: 0.123,
-                 name: "BAT",
-                 imageUrl: "https://bin.bnbstatic.com/images/20191211/b87dedcf-edaa-4069-bfad-de7f88d7204e.png"
-             },
-             stakedAsset: {
-                 USDValue: 140,
-                 name: "USDC",
-                 imageUrl: "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20201110/4cf7d633-92fb-4d37-80ed-458c7d1ea410.png"
-             },
-             url: "http://google.fr",
-         },
-         ]; */
+        let launchPoolList = [];
+
+        //Retreiving launchPoolUrl
+        launchPoolList = await parseBinanceLaunchPoolList(launchPoolList).then((res, err) => {
+            if (err)
+                return err;
+            return res;
+        });
+
+        //Retreive launchPoolPair
+        launchPoolList = await getPairByLPList(launchPoolList).then((res, err) => {
+            if (err)
+                return err;
+            return launchPoolList;
+        });
+
+        launchPoolList = await getAssetsUSDTValue(launchPoolList).then((res, err) => { return res });
+
+        console.log("LaunchPool result ")
+
+        return launchPoolList;
     }
 
-    function parseBinanceLaunchPoolList() {
-
+    //Initialization of launchPoolList with urls
+    function parseBinanceLaunchPoolList(launchPoolList) {
         return new Promise((success, failure) => {
             request({
                 method: 'GET',
@@ -78,27 +50,68 @@ exports.handler = async (event, context, callback) => {
             }, (err, res, body) => {
 
                 if (err) {
-                    console.log("ERROR DETECTED");
-                    failure({ err: "Error detected" });//console.error(err);
+                    console.err("Error while retriving LaunchPoolList : ", err);
+                    failure({ err: "Error detected" });
                 }
 
                 let $ = cheerio.load(body);
 
                 //Getting LaunchPool pages links
                 let launchPoolLinks = $('a[href^="/en/launchpool/"]');
-
                 for (var i = 0; i < launchPoolLinks.length; i++) {
-                    launchPoolLists.push({ url: BASE_URL + launchPoolLinks[i].attribs.href });
-                    //console.log("Link : ", launchPoolLists[i] );
+                    launchPoolList.push({ url: BASE_URL + launchPoolLinks[i].attribs.href });
                 }
-                //console.log("RES TEST: ", launchPoolLists);
 
-                success(launchPoolLists);
+                success(launchPoolList);
 
             });
+        });
+    }
+
+    function getPairByLPList(LPList) {
+        return new Promise(function (success, failure) {
+
+            LPList.forEach((LP) => {
+                var pair = LP.url.split('/').pop().split("_");
+                LP.stakedAsset = { name: pair.pop() };
+                LP.earnedAsset = { name: pair.shift() };
+            });
+            success(LPList);
         })
+    }
 
+    async function getAssetsUSDTValue(LPList) {
+        var assetsUSDTValues = [];
 
+        for (var i = 0, len = LPList.length; i < len; i++) {
+            if (assetsUSDTValues[LPList[i].stakedAsset.name] == null)
+                assetsUSDTValues[LPList[i].stakedAsset.name] = await getAssetValue(LPList[i].stakedAsset.name).then((res, err) => { return res });
+
+            LPList[i].stakedAsset.USDValue = assetsUSDTValues[LPList[i].stakedAsset.name];
+
+            if (assetsUSDTValues[LPList[i].earnedAsset.name] == null)
+                assetsUSDTValues[LPList[i].earnedAsset.name] = await getAssetValue(LPList[i].earnedAsset.name).then((res, err) => { return res });
+
+            LPList[i].earnedAsset.USDValue = assetsUSDTValues[LPList[i].earnedAsset.name];
+        }
+
+        return LPList;
+    }
+
+    function getAssetValue(assetSymbol) {
+        return new Promise((success, failure) => {
+
+            request({
+                method: 'GET',
+                url: API_GETPRICE_URL + assetSymbol + "USDT"
+            }, (err, res, body) => {
+                if (err) {
+                    console.err("Error while retriving %s price : %s", assetSymbol, err);
+                    failure({ err: "Error detected" });
+                }
+                success(JSON.parse(body).price);
+            });
+        });
     }
 
 };
