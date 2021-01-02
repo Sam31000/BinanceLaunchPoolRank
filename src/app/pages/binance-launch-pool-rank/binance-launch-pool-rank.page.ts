0187ba503f673b36ced0924f93e685c7b61168c6
Sam <sami.chidiac.development@gmail.com>
2021-01-02 19:36:39 +0100

    adding style on page title, titles, footer

--- src/app/pages/binance-launch-pool-rank/binance-launch-pool-rank.page.ts
+++ src/app/pages/binance-launch-pool-rank/binance-launch-pool-rank.page.ts
@@ -11,6 +11,7 @@ export class BinanceLaunchPoolRankPage implements OnInit {
 
   investValue: number = 100.00;
   launchPools: any;
+  isLoading: boolean = true
 
   constructor(private _launchPoolService : LaunchPoolService) {
   }
@@ -18,5 +19,6 @@ export class BinanceLaunchPoolRankPage implements OnInit {
   async ngOnInit() {
     //Get launchPools urls listed on Binance
     this.launchPools = await this._launchPoolService.getLaunchPoolUrl();
+    this.isLoading = false;
   }
 }
